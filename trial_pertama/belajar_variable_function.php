bb3138b261b1125d853ddd889dd021e38cdd692d
UMLearning <115670378+UMLearning@users.noreply.github.com>
2023-01-12 09:25:50 +0800

    Add files via upload

---
+++ trial_pertama/belajar_variable_function.php
@@ -0,0 +1,32 @@
+<?php
+//Cara Pertama
+$a = 'Belajar Dasar PHP';
+
+echo $a;
+
+echo "<br>";
+
+$nilai_1 = 2;
+$nilai_2 = 4;
+$hasil = $nilai_1 * $nilai_2;
+
+echo "Hasilnya adalah ".$hasil;
+
+//Cara Kedua
+function functionPertama(){
+    $a = 'Belajar Dasar PHP';
+
+    echo $a;
+
+    echo "<br>";
+
+    $nilai_1 = 2;
+    $nilai_2 = 4;
+    $hasil = $nilai_1 * $nilai_2;
+
+    echo "Hasilnya adalah ".$hasil;
+}
+
+echo "<br>";
+functionPertama();
+
